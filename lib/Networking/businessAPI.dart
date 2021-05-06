f3ec5e19cbacb88ec6eacdc526e114690effcf9a
Thossakrai Nakkasem <thossakrai@outlook.com>
2021-05-07 05:27:34 +0700

    implement businessapi

--- lib/Networking/businessAPI.dart
+++ lib/Networking/businessAPI.dart
@@ -1,15 +1,24 @@
 import 'package:http/http.dart' as http;
-class BusinessAPI {
-  Future<http.Response> listBusiness(int status, int page) async {
 
+class BusinessAPI {
+  String devServer = 'localhost:9999';
+  String commonEndpoint = '/api/v1/business';
+  Future<http.Response> listBusiness(int status, int page) async {
+    Map<String, String> queryParams = {
+      'status': status.toString(),
+      'page': page.toString()
+    };
+
+    var response =
+        await http.get(Uri.http(devServer, commonEndpoint, queryParams));
+    return response;
   }
 
   Future<http.Response> getBusiness(String businessId) async {
-
+    var response =
+        await http.get(Uri.http(devServer, commonEndpoint + '$businessId'));
+    return response;
   }
 
-  void updateBusiness(String businessId) {
-
-  }
-
+  void updateBusiness(String businessId) {}
 }
