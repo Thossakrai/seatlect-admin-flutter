77a7383e06f63f0b5152a82253a44478e4e35164
Thossakrai Nakkasem <thossakrai@outlook.com>
2021-05-07 05:03:21 +0700

    added function stup businessapi

---
+++ lib/Networking/businessAPI.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+class BusinessAPI {
+  Future<http.Response> listBusiness(int status, int page) async {
+
+  }
+
+  Future<http.Response> getBusiness(String businessId) async {
+
+  }
+
+  void updateBusiness(String businessId) {
+
+  }
+
+}
